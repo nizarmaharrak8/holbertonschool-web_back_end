d4e6af1b8ce4371cf4ab6fbf21ee1914763cc4c9
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-04-13 19:10:16 +0300

    Add variable definitions with type annotations

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines variables with annotations"""
+a: int = 1
+
+"""Defines a variable pi with the value 3.14 and an annotation of float"""
+pi: float = 3.14
+
+"""Defines a variable i_understand_annotations
+ with the value True and an annotation of bool"""
+i_understand_annotations: bool = True
+
+"""Defines a variable school with
+the value Holberton and an annotation of str"""
+school: str = "Holberton"
