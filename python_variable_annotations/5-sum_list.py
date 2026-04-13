6db0fffb81835986ed0f64c49667bd531d914617
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-04-13 19:11:22 +0300

    Add sum_list function with type annotations for input and return value

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The input list is annotated as a list of floats,
+      and the return value is annotated as a float."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """The input list is annotated as a list of floats,
+      and the return value is annotated as a float."""
+    return sum(input_list)
