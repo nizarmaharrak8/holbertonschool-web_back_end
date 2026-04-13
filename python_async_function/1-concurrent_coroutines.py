02b835990d58ddf3e3ec60fe43b88aa8525b333c
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-04-13 19:23:01 +0300

    Add wait_n coroutine to spawn wait_random multiple times and return sorted delays

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Test file for the wait_n coroutine"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay
+    and returns a list of delays in ascending order.
+    """
+    delays = []
+    for _ in range(n):
+        notdelay = await wait_random(max_delay)
+        delays.append(notdelay)
+    return sorted(delays)
