697aa704799ffd57601de501f9e6256438f3352e
root <root@ip-10-42-38-51.ec2.internal>
2026-05-02 20:56:23 +0000

    add

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
-"""Simple pagination"""
+"""Simple helper function"""
+from typing import Tuple, List
 import csv
-from typing import List, Tuple
+import math
 
 
-def index_range(page, page_size) -> Tuple[int, int]:
+def index_range(page, page_size) -> tuple[int, int]:
     """Returns a tuple of size two containing a start index and an end index"""
     start = (page - 1) * page_size
     end = start + page_size
-    return (start,  end)
+    return (start, end)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a Server instance
-        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
-    """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
-        assert isinstance(
-            page, int) and page > 0
-        assert isinstance(
-            page_size, int) and page_size > 0
+        """Returns the appropriate page of the dataset
+        (i.e. the correct list of rows)"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
         data = self.dataset()
