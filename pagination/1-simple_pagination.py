64cae8952f8a5da43114378f5a36cbf195b16616
nizarmaharrak8 <nizarmaharrak@gmail.com>
2026-05-02 16:58:56 +0000

    fix

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,49 +1,21 @@
 #!/usr/bin/env python3
-"""Simple pagination"""
-import csv
-from typing import List, Tuple
+"""
+Simple helper function for pagination.
+"""
 
 
-def index_range(page, page_size) -> Tuple[int, int]:
-    """Returns a tuple of size two containing a start index and an end index"""
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start,  end)
-
-
-class Server:
-    """Server class to paginate a database of popular baby names.
+def index_range(page: int, page_size: int) -> tuple:
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
+    Return start and end indexes for pagination.
 
-    def __init__(self):
-        """Initializes a Server instance
-        """
-        self.__dataset = None
+    Args:
+        page: current page number (1-indexed)
+        page_size: number of items per page
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-        return self.__dataset
+    Returns:
+        tuple: (start_index, end_index)
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
 
-    """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns the appropriate page of the dataset (i.e. the correct list of rows)
-                """
-        assert isinstance(
-            page, int) and page > 0
-        assert isinstance(
-            page_size, int) and page_size > 0
-
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start >= len(data):
-            return []
-        return data[start:end]
+    return (start_index, end_index)
